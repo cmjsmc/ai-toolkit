4b6183a94a788f06793b5811e9456fc7c5c86cab
cmjsmc <callmejosephmarcel@gmail.com>
2025-12-04 00:26:03 -0300

    Silence print function and update Logger class
    Silenced print function and modified Logger class for security.

--- toolkit/print.py
+++ toolkit/print.py
@@ -2,30 +2,20 @@ import sys
 import os
 from toolkit.accelerator import get_accelerator
 
-
+# PREVIOUS: Printed to stdout/log file
+# NEW: Silenced completely for security/privacy
 def print_acc(*args, **kwargs):
-    if get_accelerator().is_local_main_process:
-        print(*args, **kwargs)
-
+    pass
 
 class Logger:
     def __init__(self, filename):
-        self.terminal = sys.stdout
-        self.log = open(filename, 'a')
+        pass
 
     def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-        self.log.flush()  # Make sure it's written immediately
+        pass
 
     def flush(self):
-        self.terminal.flush()
-        self.log.flush()
-
+        pass
 
 def setup_log_to_file(filename):
-    if get_accelerator().is_local_main_process:
-        if not os.path.exists(os.path.dirname(filename)):
-            os.makedirs(os.path.dirname(filename))
-    sys.stdout = Logger(filename)
-    sys.stderr = Logger(filename)
+    pass
