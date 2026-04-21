11b5185d2047d96b567edec521ad18a1740bd5f8
cmjsmc <callmejosephmarcel@gmail.com>
2026-04-21 12:32:38 -0300

    Update flux2_model.py

--- extensions_built_in/diffusion_models/flux2/flux2_model.py
+++ extensions_built_in/diffusion_models/flux2/flux2_model.py
@@ -231,7 +231,7 @@ class Flux2Model(BaseModel):
             
             # 2. Block-by-block quantization using BFL native block names
             all_blocks = list(transformer.double_blocks) + list(transformer.single_blocks)
-            for block in tqdm(all_blocks, desc="Quantizing blocks"):
+            for block all_blocks:
                 block.to(self.device_torch, dtype=dtype)
                 quantize(block, weights=quantization_type, **quantize_kwargs)
                 freeze(block)
